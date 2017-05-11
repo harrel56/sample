9df9f4d6963a2d28f6e46840b3263145a4ca8ded
harrel56 <marcin.slowik95@gmail.com>
2017-05-11 21:16:16 +0200

    doc added

---
+++ doc/matrices/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['createnewmatrix',['createNewMatrix',['../main_8cpp.html#a29769de8838dc6ca27b3582178fb1bbf',1,'main.cpp']]]
+];
---
+++ doc/matrices/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['elements',['elements',['../class_matrix.html#a9235a708bef6650507ffed4c6c708289',1,'Matrix']]]
+];
